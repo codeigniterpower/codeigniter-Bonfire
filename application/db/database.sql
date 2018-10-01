412859a8f612f29f6582e80ec00f43d55565a2cc
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2018-09-30 23:05:10 -0400

    provide the started database need for run the application

---
+++ application/db/database.sql
@@ -0,0 +1,11 @@
+
+CREATE DATABASE `bonfire` DEFAULT CHARACTER SET utf8 ;
+
+
+CREATE TABLE IF NOT EXISTS `bonfire`.`bf_ci3_sessions` (
+        `id` varchar(128) NOT NULL,
+        `ip_address` varchar(45) NOT NULL,
+        `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        `data` blob NOT NULL,
+        KEY `ci_sessions_timestamp` (`timestamp`)
+);
